d32e66462ad2d3b2ffe6565692c8fa779584627a
Dale Salangoste <dalesalangoste@gmail.com>
2025-04-13 22:39:09 -0500

    Add breed collection sorting (Feature 3.5)
    - Add sort options for breeds by:
      - Name (A-Z and Z-A)
      - Group
      - Origin
    - Maintain sort during search
    - Show current sort selection
    - Default sort by name ascending

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -5,6 +5,19 @@ class BreedsController < ApplicationController
     else
       Breed.all
     end
+
+    @breeds = case params[:sort]
+      when 'name_asc'
+        @breeds.order(name: :asc)
+      when 'name_desc'
+        @breeds.order(name: :desc)
+      when 'group'
+        @breeds.order(:group)
+      when 'origin'
+        @breeds.order(:origin)
+      else
+        @breeds.order(:name)
+    end
   end
 
   def show
