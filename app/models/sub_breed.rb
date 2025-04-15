801fdb5d578836c7023aed0a046fed435a96db5b
Dale Salangoste <dalesalangoste@gmail.com>
2025-04-15 17:56:37 -0500

    Model validations confirmed 1.6

--- app/models/sub_breed.rb
+++ app/models/sub_breed.rb
@@ -2,7 +2,7 @@ class SubBreed < ApplicationRecord
   belongs_to :breed
   has_many :dog_images, dependent: :destroy
   
-  validates :name, presence: true, uniqueness: { scope: :breed_id }
+  validates :name, presence: true, uniqueness: { scope: :breed_id, message: "should be unique for each breed" }
   validates :slug, presence: true, uniqueness: true
   validates :breed_id, presence: true
 end
